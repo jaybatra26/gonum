c089267bc8a135e23e545410f8afe34d6383f482
Dan Kortschak <dan@kortschak.io>
2019-03-09 14:19:41 +1030

    unit/constant: new package for dimensioned fundamental constants

---
+++ unit/constant/doc.go
@@ -0,0 +1,8 @@
+// Copyright ©2019 The Gonum Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:generate go run generate_constants.go
+
+// Package constant provides fundamental constants satisfying unit.Uniter.
+package constant
